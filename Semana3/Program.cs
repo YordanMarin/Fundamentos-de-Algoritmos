92b25c248ef70d0e6a67248a284cc8ecd36a7783
yordan <yordanmarinsovero@gmail.com>
2025-04-10 11:50:27 -0500

    ejercicio1

---
+++ Semana3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio1();
+            Console.ReadKey();
+        }
+
+        static void ejercicio1()
+        {
+            string nombre;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("\n"+nombre+", Bienvenido al curso de Fundamentos de Algortimo");
+        }
+    }
+}
