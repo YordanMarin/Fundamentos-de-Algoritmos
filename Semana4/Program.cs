d2a53f9d4f28f71e08ba38d5ff6c1daf5d841488
yordan <yordanmarinsovero@gmail.com>
2025-04-14 13:55:04 -0500

    Avance 1 -semana4

---
+++ Semana4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio3();
+            Console.ReadKey();
+        }
+
+        static void ejercicio1()
+        {
+            int num1;
+
+            Console.Write("Ingrese un número entero: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 %2==0)
+            {
+                Console.WriteLine("\nEl número es par");
+            }
+            else
+            {
+                Console.WriteLine("\nEl número es impar");
+            }
+        }
+
+        static void ejercicio2()
+        {
+            int edad;
+
+            Console.Write("Ingrese una edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad < 0)
+            {
+                Console.WriteLine("\nEdad inválida!");
+            }
+            else
+            {
+                if (edad < 18)
+                {
+                    Console.WriteLine("\nEs menor de edad");
+                }
+                else
+                {
+                    Console.WriteLine("\nEs mayor de edad");
+                }
+            }
+        }
+
+        static void ejercicio3()
+        {
+
+        }
+    }
+}
