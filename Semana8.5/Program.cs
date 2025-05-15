e83afd7e627c67b2c2c003aad3d3ed52e49fc5e0
yordan <yordanmarinsovero@gmail.com>
2025-05-15 12:18:21 -0500

    avance sem8

---
+++ Semana8.5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana8._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            char continuar;
+
+            do {
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicación");
+                Console.WriteLine("4. División");
+                Console.WriteLine("0. Salir");
+
+                Console.Write("\nIngrese una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+                Console.Clear();
+            } while (opcion <0 || opcion >=5);
+        }
+
+        static void suma(int x, int y) {
+
+        }
+
+        static void resta(int x, int y) {
+
+        }
+
+        static void multi(int x, int y) {
+
+        }
+
+        static void divi(int x, int y) {
+
+        }
+    }
+}
---
+++ Semana8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma: " + ejer4());
+            //ejer3();
+            //Console.Write("Ingrese número 1: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("Ingrese número 2: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\nSuma: " + ejer2(num1,num2));
+
+            Console.ReadKey();
+        }
+
+        static void ejer1(int num1, int num2) {
+            Console.Write("Ingrese número 1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese número 2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSuma: " + (num1 + num2));
+        }
+
+        static int ejer2(int num1, int num2) {
+            return num1 + num2;
+        }
+
+        static void ejer3() {
+            Console.Write("Ingrese número 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese número 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSuma: " + (num1 + num2));
+        }
+
+        static int ejer4() {
+            Console.Write("Ingrese número 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese número 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            return num2 + num1;
+        }
+    }
+}
