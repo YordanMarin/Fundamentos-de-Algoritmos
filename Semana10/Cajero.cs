f86e5251dadb1bafe9165958a6f683a615cd262d
Yordan Marin <119029706+YordanMarin@users.noreply.github.com>
2025-05-29 12:03:15 -0500

    Add files via upload

---
+++ Semana10/Cajero.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana10
+{
+    class Cajero
+    {
+        private double saldo = 1000;
+
+        public double consultar() {
+            return saldo;
+        }
+
+        public void depositar(double monto) {
+            do {
+                Console.Write("¿Cuánto desea depositar?: ");
+                string entrada = Console.ReadLine();
+
+                if(double.TryParse(entrada, out monto)) {
+                    if(monto > 0) {
+                        saldo += monto;
+                        Console.WriteLine("Depósito exitoso.");
+                        break;
+                    }else
+                        Console.WriteLine("Error: solo se puede depositar montos mayores a 0");
+                } else {
+                    Console.WriteLine("Error: solo se permiten números!");
+                }
+
+            } while (true);
+        }
+
+        public void retirar(double monto) {
+            do {
+                Console.Write("¿Cuánto desea retirar?: ");
+                string entrada = Console.ReadLine();
+
+                if (double.TryParse(entrada, out monto)) {
+                    if (monto > saldo)
+                        Console.WriteLine("No tiene saldo suficiente");
+                    else if (monto < 0)
+                        Console.WriteLine("Error: solo se puede retirar montos mayores o iguales 0");
+                    else {
+                        saldo -= monto;
+                        Console.WriteLine("Retiro exitoso.");
+                        break;
+                    }
+                } else {
+                    Console.WriteLine("Error: solo se permiten números!");
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Semana10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcion, continuar;
+            Cajero c = new Cajero();
+
+            do {
+                do {
+                    Console.WriteLine("BIENVENIDOS AL SISTEMA DE CAJERO\n");
+
+                    Console.WriteLine("1. Consultar saldo");
+                    Console.WriteLine("2. Depositar dinero");
+                    Console.WriteLine("3. Retirar dinero");
+                    Console.WriteLine("4. Salir\n");
+
+                    Console.Write("Ingrese una opción: ");
+                    opcion = Console.ReadKey().KeyChar;
+                    Console.Clear();
+
+                } while (opcion < '1' || opcion > '4');
+
+                switch (opcion) {
+                    case '1': Console.WriteLine("Saldo: S/."+c.consultar()); break;
+                    case '2': c.depositar(0); break;
+                    case '3': c.retirar(0); break;
+                    case '4': Console.Write("Cerrando sistema!"); return;
+                }
+                do {
+                    Console.Write("\nDesea regresar al menú (S/N): ");
+                    continuar = char.ToUpper(Console.ReadKey().KeyChar);
+                    Console.WriteLine();
+
+                    if (continuar != 'S' && continuar != 'N')
+                        Console.WriteLine("Opción no valida! Ingrese solo S/N: ");
+
+                } while (continuar != 'S' && continuar != 'N');
+                Console.Clear();
+
+            } while (continuar == 'S');
+            
+        }
+    }
+}
