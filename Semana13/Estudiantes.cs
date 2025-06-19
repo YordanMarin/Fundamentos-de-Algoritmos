d549d5ebab27c3883ee2284301f6ee9f1459ea78
“bojorquezja” <bojorquez.ja@gmail.com>
2025-06-19 12:09:37 -0500

    semana13

---
+++ Semana13/Estudiantes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana13
+{
+    class Estudiantes
+    {
+        private string[] nombres = new string[0];
+        private byte[] edad = new byte[0];
+        private int cantidad = 0;
+
+        public void insertar() {
+            string continuar;
+            do {
+                Console.Clear();
+
+                Console.WriteLine("=== Registrar estudiantes ===\n");
+
+                Array.Resize(ref nombres, nombres.Length + 1);
+                Array.Resize(ref edad, edad.Length + 1);
+
+                Console.Write("Ingrese sus nombres: ");
+                nombres[cantidad] = Console.ReadLine();
+                Console.Write("Ingrese su edad: ");
+                edad[cantidad] = byte.Parse(Console.ReadLine());
+                cantidad++;
+
+                Console.Write("\nSe registro con exito! Desea seguir registrando (S): ?");
+                continuar = Console.ReadLine().ToLower();
+            } while (continuar == "s");
+        }
+
+        public void mostrar() {
+            Console.Clear();
+            Console.WriteLine("=== Estudiantes registrados ===\n");
+            if (cantidad != 0) {
+                for (int i = 0; i < nombres.Length; i++) {
+                    Console.WriteLine($"{i + 1}. {nombres[i]}\t{edad[i]}");
+                }
+            } else
+                Console.WriteLine("\nNo hay registros de estudiantes!");
+        }
+
+        public int buscar(string nom) {
+            int indice = -1;
+            for (int i = 0; i < nombres.Length; i++) {
+                if (nombres[i] == nom) {
+                    indice = i;
+                }
+            }
+            return indice;
+        }
+
+        public void modificar() {
+            Console.Clear();
+            Console.WriteLine("=== Modificar estudiante ===\n");
+            Console.Write("Ingrese el nombre: ");
+            string ingreso = Console.ReadLine();
+
+            int indice = buscar(ingreso);
+
+            if (indice != -1) {
+                Console.Write("\nIngrese el nuevo nombre: ");
+                nombres[indice] = Console.ReadLine();
+                Console.Write("Ingrese la nueva edad: ");
+                edad[indice] = byte.Parse(Console.ReadLine());
+                Console.WriteLine("\nEstudiante modificado correctamnete!");
+            } else {
+                Console.WriteLine("\nNo se puede modificar. Estudiante no existe!");
+            }
+        }
+    }
+}
---
+++ Semana13/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            Estudiantes e = new Estudiantes();
+            do {
+                do {
+                    Console.Clear();
+                    Console.WriteLine("=== MENÚ DE GESTIÓN DE ESTUDIANTES ===");
+                    Console.WriteLine("1. Registrar");
+                    Console.WriteLine("2. Mostrar");
+                    Console.WriteLine("3. Buscar");
+                    Console.WriteLine("4. Modificar");
+                    Console.WriteLine("5. Eliminar");
+                    Console.WriteLine("6. Ordenar");
+                    Console.WriteLine("7. Salir\n");
+                    Console.Write("Seleccione una opción: ");
+                    string entrada = Console.ReadLine();
+                    if (!int.TryParse(entrada, out opcion) || opcion < 1 || opcion > 7) {
+                        Console.Write("\nEntrada inválida. Presione una tecla para intentarlo nuevamente...");
+                        Console.ReadKey();
+                    }
+                } while (opcion < 1 || opcion > 7);
+                switch (opcion) {
+                    case 1: e.insertar(); break;
+                    case 2: e.mostrar(); break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("=== Buscar estudiante ===\n");
+                        Console.Write("Ingrese el nombre a buscar: ");
+                        string ingreso = Console.ReadLine();
+
+                        int indice = e.buscar(ingreso);
+
+                        if (indice != -1)
+                            Console.WriteLine($"Estudiante {ingreso} esta en la posición {indice+1}");
+                        else
+                            Console.WriteLine($"Estudiante {ingreso} no existe");
+
+                            break;
+                    case 4: e.modificar(); break;
+                    case 5: break;
+                    case 6: break;
+                    case 7: Console.WriteLine("\nSaliendo del programa..."); return;
+                }
+                if (opcion != 7) {
+                    Console.Write("\nPresione una tecla para regresar al menú...");
+                    Console.ReadKey();
+                }
+            } while (opcion != 7);
+        }
+    }
+}
